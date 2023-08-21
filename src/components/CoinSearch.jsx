190bdf70b1752e377d2ea46673c3cc03ea2eb185
goku-raj <anshuraj1479@gmail.com>
2023-08-21 15:30:37 +0530

    Coin Search board

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,41 +1,54 @@
-import React from 'react'
-import CoinItem from './CoinItem';
+import React, { useState } from "react";
+import CoinItem from "./CoinItem";
 
-const CoinSearch = ({coins}) => {
+const CoinSearch = ({ coins }) => {
+  const [searchText, setSearchText] = useState("");
   // console.log(coins)
   return (
-    <div>
-      <div>
-          <h1>Search Crypto</h1>
-          <form>
-            <input type="text" placeholder='Search a coin' />
-          </form>
+    <div className="rounded-div my-4">
+      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+        <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
+        <form>
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl"
+            type="text"
+            placeholder="Search a coin"
+          />
+        </form>
       </div>
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>#</th>
-          <th>Coin</th>
-          <th></th>
-          <th>Price</th>
-          <th>24hr</th>
-          <th>24hr Volume</th>
-          <th>Mkt</th>
-          <th>Last 7 Days</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          coins.map((coin)=>(
-            <CoinItem key={coin.id} coin={coin}/>
-          ))
-        }
-      </tbody>
-    </table>
-
+      <table className="w-full border-collapse text-center ">
+        <thead>
+          <tr className="border-b">
+            <th></th>
+            <th className="px-4">#</th>
+            <th className="text-left">Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24hr</th>
+            <th className="hidden md:table-cell">24hr Volume</th>
+            <th className="hidden sm:table-cell">Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins
+            .filter((value) => {
+              if (searchText === "") {
+                return value;
+              } else if (
+                value.name.toLowerCase().includes(searchText.toLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((coin) => (
+              <CoinItem key={coin.id} coin={coin} />
+            ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default CoinSearch
+export default CoinSearch;
