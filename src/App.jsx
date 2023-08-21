3fd79798535cc52c0557b769a981339ac6874466
goku-raj <anshuraj1479@gmail.com>
2023-08-21 18:20:23 +0530

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Account from './routes/Account';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import CoinPage from "./routes/CoinPage";
+import Footer from "./components/Footer";
 
 
 
@@ -35,6 +36,7 @@ function App() {
           <Route path=":coinId"/>
         </Route>
       </Routes>
+      <Footer/>
     </ThemeProvider>
   )
 }
