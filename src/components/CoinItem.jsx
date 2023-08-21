68b61ab10f25de07f5fed0bf5ffbf8108d425631
goku-raj <anshuraj1479@gmail.com>
2023-08-21 18:06:49 +0530

    Coin Detail Page

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AiOutlineStar } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinItem = ({ coin }) => {
@@ -10,26 +11,36 @@ const CoinItem = ({ coin }) => {
       </td>
       <td>{coin.market_cap_rank}</td>
       <td>
-        <div className="flex items-center">
-          <img
-            className="w-6 mr-2 rounded-full"
-            src={coin.image}
-            alt={coin.name}
-          />
-          <p className="hidden sm:table-cell">{coin.name}</p>
-        </div>
+        <Link to={`/coin/${coin.id}`}>
+          <div className="flex items-center">
+            <img
+              className="w-6 mr-2 rounded-full"
+              src={coin.image}
+              alt={coin.name}
+            />
+            <p className="hidden sm:table-cell">{coin.name}</p>
+          </div>
+        </Link>
       </td>
       <td>{coin.symbol.toUpperCase()}</td>
-      <td>₹{coin.current_price.toLocaleString('en-IN')}</td>
+      <td>₹{coin.current_price.toLocaleString("en-IN")}</td>
       <td>
         {coin.price_change_percentage_24h > 0 ? (
-          <p className="text-green-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>
+          <p className="text-green-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
         ) : (
-          <p className="text-red-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>
+          <p className="text-red-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
         )}
       </td>
-      <td className="w-[180px] hidden md:table-cell">₹{coin.total_volume.toLocaleString('en-IN')}</td>
-      <td className="w-[180px] hidden sm:table-cell">₹{coin.market_cap.toLocaleString('en-IN')}</td>
+      <td className="w-[180px] hidden md:table-cell">
+        ₹{coin.total_volume.toLocaleString("en-IN")}
+      </td>
+      <td className="w-[180px] hidden sm:table-cell">
+        ₹{coin.market_cap.toLocaleString("en-IN")}
+      </td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color="teal" />
